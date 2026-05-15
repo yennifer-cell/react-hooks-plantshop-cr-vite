77bd9b09ef7c1b5d48237c73f5f0a9dad94c2eef
yennifer <shiksjayne95@gmail.com>
2026-05-15 19:39:28 +0300

    Refactor App component to include PlantCard

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,26 @@
-import React from "react";
-import Header from "./Header";
-import PlantPage from "./PlantPage";
+import { useState } from "react";
 
-function App() {
+function PlantCard({ plant }) {
+  const [inStock, setInStock] = useState(true);
+
+  function handleStockToggle() {
+    setInStock(!inStock);
+  }
+
   return (
-    <div className="app">
-      <Header />
-      <PlantPage />
-    </div>
+    <li className="card" data-testid="plant-item">
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      {inStock ? (
+        <button className="primary" onClick={handleStockToggle}>
+          In Stock
+        </button>
+      ) : (
+        <button onClick={handleStockToggle}>Out of Stock</button>
+      )}
+    </li>
   );
 }
 
-export default App;
+export default PlantCard;
